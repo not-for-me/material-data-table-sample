dd6a3cdcac76927871bda2b823c1434e58325ac4
NFM <jwj0831@gmail.com>
2017-08-01 09:12:01 +0900

    add a material default example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdTableModule, MdPaginatorModule } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CdkTableModule,
+    MdTableModule,
+    MdPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
